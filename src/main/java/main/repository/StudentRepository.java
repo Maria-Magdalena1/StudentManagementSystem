3c693ada3d34b90c7f96a6ca2db020b427aef497
Maria-Magdalena1 <mariamagdalena.vladimirova@gmail.com>
2026-07-21 22:17:37 +0300

    remove unused methods

--- src/main/java/main/repository/StudentRepository.java
+++ src/main/java/main/repository/StudentRepository.java
@@ -4,11 +4,12 @@ import main.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface StudentRepository extends JpaRepository<Student,Long> {
-    //Optional<Student> findByFacultyNumber(String facultyNumber);
-//
-    //Optional<Student> findByEmail(String email);
-//
-    //List<Student> findByLastNameContainingIgnoreCase(String lastName);
+public interface StudentRepository extends JpaRepository<Student, Long> {
+    Optional<Student> findByEmail(String email);
+
+    Optional<Student> findByFacultyNumber(Integer facultyNumber);
+
 }
